560a311ad5f9c9f315861720912a73cb3cd136bb
thefipster <felix.reisch@live.de>
2021-07-12 01:02:23 +0200

    added missing service.

---
+++ src/spa/src/app/modules/seed-search/services/api/api.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SeedModel } from '../../interfaces/seed-model';
+import { SeedSearchModel } from '../../interfaces/seed-search-model';
+import { FilterService } from '../filter/filter.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private apiUrl: string = 'https://localhost:44346/api/seeds';
+
+  constructor(
+    private http: HttpClient,
+    private filterService: FilterService
+  ) { }
+
+  public searchSeeds(): Observable<SeedModel[]> {
+    const search = this.filterService.getSearch();
+    return this.http.post<SeedModel[]>(this.apiUrl, search);
+  }
+}
