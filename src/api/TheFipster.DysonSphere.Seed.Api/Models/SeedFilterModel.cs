61db5ce8edaf03af37f4a00b3058d306bbd2c576
thefipster <felix.reisch@live.de>
2021-07-12 00:50:25 +0200

    added missing files.

---
+++ src/api/TheFipster.DysonSphere.Seed.Api/Models/SeedFilterModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TheFipster.DysonSphere.Seed.Api.Models
+{
+    public class SeedFilterModel
+    {
+        public string Column { get; set; }
+        public double Min { get; set; }
+        public double Max { get; set; }
+    }
+}
